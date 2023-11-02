62f8f4393fa204ba02e3ac221427016cfb36720a
DangKhoa2233 <92931242+DangKhoa2233@users.noreply.github.com>
2023-11-02 12:41:07 +0700

    update code

--- src/page/admin/page/RoomManagement/BookManagement.js
+++ src/page/admin/page/RoomManagement/BookManagement.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Confirm } from '../../../../component/Confirm/Confirm';
+import React, { useEffect, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Confirm } from "../../../../component/Confirm/Confirm";
 // import { FormCreate } from '../../shareComponent/form/FormCreate';
 // import { FormUpdate } from '../../shareComponent/form/FormUpdate';
 // import { ModalCreate } from '../../shareComponent/modal/ModalCreate';
-import { TableMain } from '../../shareComponent/table/TableMain'
-import { useDispatch } from 'react-redux';
-import { deleteRoom } from '../../../../redux/action/phongAction';
-import { columns, getDataTable } from './helper';
-import { ModalEditBook } from './component/modal/ModalEditBook';
-import { ModalCreateRoom } from './component/modal/ModalCreateRoom';
-import { setConfirm } from '../../../../redux/action/homeAction';
-import Swal from 'sweetalert2';
-import { ModalEditReaction } from './component/modal/ModalEditReaction';
-import { getCommonCode } from 'redux/action/getCommonCode';
-import useGetDataBook from './hook/useGetDataBook';
-import useGetDetailBook from './hook/useGetDetailBook';
-import useLoadingEffect from 'fuse/hook/useLoadingEffect';
+import { TableMain } from "../../shareComponent/table/TableMain";
+import { useDispatch } from "react-redux";
+import { deleteRoom } from "../../../../redux/action/phongAction";
+import { columns, getDataTable } from "./helper";
+import { ModalEditBook } from "./component/modal/ModalEditBook";
+import { ModalCreateRoom } from "./component/modal/ModalCreateRoom";
+import { setConfirm } from "../../../../redux/action/homeAction";
+import Swal from "sweetalert2";
+import { ModalEditReaction } from "./component/modal/ModalEditReaction";
+import { getCommonCode } from "redux/action/getCommonCode";
+import useGetDataBook from "./hook/useGetDataBook";
+import useGetDetailBook from "./hook/useGetDetailBook";
+import useLoadingEffect from "fuse/hook/useLoadingEffect";
 
 export const BookManagement = () => {
-
   // State
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalEditOpen, setIsModalEditOpen] = useState(false);
@@ -36,39 +35,35 @@ export const BookManagement = () => {
     await dispatch(getCommonCode("theLoai"));
     await dispatch(getCommonCode("nhaXuatBan"));
     await dispatch(getCommonCode("nhaCungCap"));
-  }, [])
+  }, []);
 
+  const { sachData, isDataLoading, fetchData, isFetching } = useGetDataBook(
+    "0",
+    "0"
+  );
 
-  const { sachData, isDataLoading, fetchData, isFetching } =
-    useGetDataBook(
-      "0",
-      "0",
-    );
-
-  const { sachDataDetail, isDataDetailLoading, fetchData: fetchDetail, isFetching: isFetchingDetail } =
-    useGetDetailBook(
-      "0",
-      "0",
-      dataEdit?._id,
-    );
-
+  const {
+    sachDataDetail,
+    isDataDetailLoading,
+    fetchData: fetchDetail,
+    isFetching: isFetchingDetail,
+  } = useGetDetailBook("0", "0", dataEdit?._id);
 
   // Method
   const handleAdd = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const handleEdit = (data) => {
     setDataEdit(data);
     setIsModalEditOpen(true);
-  }
+  };
 
   const handleDelete = async (data) => {
     // await dispatch(setConfirm({
     //   status: 'open',
     //   method: async () => {
     //     dispatch(deleteRoom(data)).then(data => {
-
     //     }).catch(err => {
     //       Swal.fire({
     //         icon: 'error',
@@ -79,22 +74,29 @@ export const BookManagement = () => {
     //       })
     //     });
     //   }
-
     // }))
-  }
+  };
 
   const handleViewDanhGia = (data) => {
     setDataEdit(data);
     setIsModalEditReaction(true);
-  }
+  };
 
   useLoadingEffect(isDataLoading, isDataDetailLoading);
 
   return (
     <>
       <div className="h-[12%] flex justify-between items-center">
-        <h3 className="text-[20px] text-[#3790c7] font-bold">{t('Book Management')}</h3>
-        <button className='flex items-center justify-center bg-[#3790c7] text-white py-[10px] px-[20px] rounded-[7px] duration-300 hover:shadow-[#3790c7a6] hover:shadow-lg hover:translate-y-[-3px]' type="submit" onClick={handleAdd}>{t('add')}</button>
+        <h3 className="text-[20px] text-[#3790c7] font-bold">
+          {t("Quản lý sách")}
+        </h3>
+        <button
+          className="flex items-center justify-center bg-[#3790c7] text-white py-[10px] px-[20px] rounded-[7px] duration-300 hover:shadow-[#3790c7a6] hover:shadow-lg hover:translate-y-[-3px]"
+          type="submit"
+          onClick={handleAdd}
+        >
+          {t("add")}
+        </button>
       </div>
       <div className="h-[88%]">
         <TableMain
@@ -106,18 +108,16 @@ export const BookManagement = () => {
       </div>
       <ModalEditBook
         methodCancel={() => setIsModalEditOpen(false)}
-        title={t('Edit Book Management')}
+        title={t("Sửa sách")}
         isOpen={isModalEditOpen}
         dataEdit={sachDataDetail}
         fetcher={fetchDetail}
         fetch={fetchData}
-        childrenForm={
-          <></>
-        }
+        childrenForm={<></>}
       />
       <ModalCreateRoom
         methodCancel={() => setIsModalOpen(false)}
-        title={t('Create Book Management')}
+        title={t("Thêm sách")}
         isOpen={isModalOpen}
         fetcher={fetchDetail}
         fetch={fetchData}
@@ -125,10 +125,10 @@ export const BookManagement = () => {
       <ModalEditReaction
         isOpen={isModalEditReaction}
         methodCancel={() => setIsModalEditReaction(false)}
-        title={t('Edit Reaction')}
+        title={t("Edit Reaction")}
         idRoom={dataEdit?.id}
       />
       <Confirm />
     </>
-  )
-}
+  );
+};
--- src/page/admin/page/RoomManagement/helper.js
+++ src/page/admin/page/RoomManagement/helper.js
@@ -1,50 +1,61 @@
-    export const columns = (onClickFuncc) => {
-        return [
-            {
-                title: 'Tên sách',  
-                dataIndex: 'tenSach',
-                key: 'tenSach',
-                width: '35%',
-            },
-            {
-                title: 'Thể loại',
-                dataIndex: 'theLoai',
-                key: 'theLoai',
-                width: '15%',
-            },
-            {
-                title: 'Số lượng',
-                dataIndex: 'soLuong',
-                key: 'soLuong',
-                width: '15%'
-            },
-            {
-                title: 'Tác giả',
-                dataIndex: 'tacGia',
-                key: 'tacGia',
-                width: '20%',
-            },
-        ]
-    }
+export const columns = (onClickFuncc) => {
+  return [
+    {
+      title: "Tên sách",
+      dataIndex: "tenSach",
+      key: "tenSach",
+      width: "25%",
+    },
+    {
+      title: "Thể loại",
+      dataIndex: "theLoai",
+      key: "theLoai",
+      width: "15%",
+    },
+    {
+      title: "Số lượng",
+      dataIndex: "soLuong",
+      key: "soLuong",
+      width: "8%",
+    },
+    {
+      title: "Tác giả",
+      dataIndex: "tacGia",
+      key: "tacGia",
+      width: "20%",
+    },
+    {
+      title: "Nhà cung cấp",
+      dataIndex: "nhaCungCap",
+      key: "nhaCungCap",
+      width: "12%",
+    },
+    {
+      title: "Nhà xuất bản",
+      dataIndex: "nhaXuatBan",
+      key: "nhaXuatBan",
+      width: "12%",
+    },
+  ];
+};
 
-    // getDataTable
-    export const getDataTable = (data) => {
-        const dataResult = [];
-        (data?.length > 0) 
-        &&
-        data.map((item, index)=> {
-        const obj = {...item, key: index}
-        return dataResult.push(obj);
-        })
-        return dataResult;
-    }
+// getDataTable
+export const getDataTable = (data) => {
+  const dataResult = [];
+  data?.length > 0 &&
+    data.map((item, index) => {
+      const obj = { ...item, key: index };
+      return dataResult.push(obj);
+    });
+  return dataResult;
+};
 
-    export const setGridColumn = (size) => {
-        if(size === '3') {
-            return 'col-span-3'
-        } else if (size === '2') {
-            return 'col-span-2'
-        } else {
-            return 'col-span-1'
-        }
-    }
+export const setGridColumn = (size) => {
+  if (size === "3") {
+    return "col-span-3";
+  } else if (size === "2") {
+    return "col-span-2";
+  } else {
+    return "col-span-1";
+  }
+};
