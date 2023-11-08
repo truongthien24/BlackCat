d4b57160bf81be812a40c8e04701ee8a0495ff91
truongthien2411 <truongthien2411@gmail.com>
2023-11-08 16:36:28 +0700

    update code

--- src/page/user/page/cart/Cart.js
+++ src/page/user/page/cart/Cart.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div className='pt-[150px] min-h-[calc(100vh_-_300px)]'>
-        <div className='bg-[#eaeaea] min-h-[calc(100%_-_150px)]'>Cart</div>
+    <div className='pt-[150px] min-h-[calc(100vh_-_300px)] flex justify-center'>
+        <div className='bg-[#eaeaea] min-h-[calc(100%_-_150px)] p-[20px] w-full xl:w-[90%] 2xl:w-[70%]'>
+            <h2>Giỏ hàng của bạn</h2>
+        </div>
     </div>
   )
 }
