598e241ae6d387c19515a094e8ad42a85b6dec55
truongthien2411 <truongthien2411@gmail.com>
2023-11-13 17:13:26 +0700

    update code

--- src/page/user/page/payment/Payment.js
+++ src/page/user/page/payment/Payment.js
@@ -5,16 +5,34 @@ import {
   SolutionOutlined,
 } from "@ant-design/icons";
 import React from "react";
+import { useParams } from "react-router-dom";
+import InfoPayment from "./components/InfoPayment";
+import MethodPayment from "./components/MethodPayment";
+import ConfirmPayment from "./components/ConfirmPayment";
 
-const Payment = ({ data }) => {
+const Payment = () => {
+
+  const {id} = useParams();
 
   const renderStepContent = () => {
-
+    const step = 'info';
+    switch(step) {
+      case 'info': {
+        return <InfoPayment/>
+      }
+      case 'payment': {
+        return <MethodPayment/>
+      }
+      case 'confirm': {
+        return <ConfirmPayment/>
+      }
+      default: return <></>
+    }
   };
-  
+
   return (
     <div className="md:pt-[150px] pb-[20px] min-h-[calc(100vh_-_300px)] flex justify-center">
-      <div className="flex flex-col items-center bg-[#eaeaea] w-full xl:w-[90%] 2xl:w-[70%] px-[25px] py-[20px]">
+      <div className="flex flex-col items-center bg-[#eaeaea] w-[95%] xl:w-[90%] 2xl:w-[70%] px-[25px] py-[20px]">
         <h3 className="mb-[30px] text-[20px]">Thanh toán</h3>
         <div className="w-full">
           <Steps
@@ -36,7 +54,7 @@ const Payment = ({ data }) => {
               },
             ]}
           />
-          <div>{renderStepContent()}</div>
+          <div className="pt-[30px]">{renderStepContent()}</div>
         </div>
       </div>
     </div>
---
+++ src/page/user/page/payment/components/InfoPayment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InfoPayment = () => {
+  return (
+    <div>InfoPayment</div>
+  )
+}
+
+export default InfoPayment
