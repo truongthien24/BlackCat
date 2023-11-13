d2fd7281aec13721d6b464134f44489ffa013d9b
truongthien24 <truongthien2411@gmail.com>
2023-11-14 00:53:59 +0700

    insert search area

--- src/page/user/component/FormSearch.js
+++ src/page/user/component/FormSearch.js
@@ -1,16 +1,20 @@
 import { Icon } from 'assets/icon';
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import { FormProvider, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { setLoading } from 'redux/action/homeAction';
 import { COLOR } from '../shareComponent/constant';
 import toast from 'react-hot-toast';
+import useFindDataBook from 'page/admin/page/RoomManagement/hook/useFindBook';
+import { Empty } from 'antd';
 
 const FormSearch = (props) => {
     // Props
     const { data } = props;
 
     const searchResultRef = useRef(null);
+
+
 
     const dispatch = useDispatch();
 
@@ -23,7 +27,29 @@ const FormSearch = (props) => {
         mode: 'onSubmit'
     })
 
+
     const { reset, register, setValue, getValues, watch, handleSubmit } = method;
+
+    const { sachData, isDataLoading, fetchData, isFetching } = useFindDataBook(
+        {
+            tenSach: watch('valueSearch')
+        }
+    )
+
+    const renderResult = () => {
+        if(sachData?.length > 0) {
+            return sachData?.map((sach, index) => {
+                return <div className="flex">
+                    <img src={sach?.hinhAnh?.url} className="w-[45px] h-full mr-[10px]" />
+                    <div>
+                        <h5 className="text-[13px]">{sach?.tenSach}</h5>
+                        <span className="text-[12px] text-[gray]">{sach?.tenTheLoai}</span>
+                    </div>
+                </div>
+            })
+        }
+        return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+    }
 
     const onSubmit = (data) => {
         dispatch(setLoading({
@@ -41,14 +67,16 @@ const FormSearch = (props) => {
     return (
         <FormProvider {...method}>
             <form className={`min-w-[350px] border-[2px] border-[#498374] border-solid rounded-[5px] relative`} onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" className="w-full px-[20px] py-[8px] text-[12px] outline-none" placeholder='Nhập từ khoá tìm kiếm' {...register('valueSearch')} onKeyDown={()=> {
+                <input type="text" className="w-full px-[20px] py-[8px] text-[12px] outline-none" placeholder='Nhập từ khoá tìm kiếm' {...register('valueSearch')} onKeyDown={(e) => {
                     searchResultRef.current.style.display = 'block'
-                }}/>
+                }} />
                 <button className={`absolute right-[-10px] top-[50%] translate-y-[-50%] bg-[#498374] w-[50px] h-[50px] flex items-center justify-center text-[#fff] rounded-[50%] border-none hover:shadow-md hover:shadow-[#498374] duration-200`}>
                     <Icon name="search" />
                 </button>
-                <div ref={searchResultRef} className="absolute top-[120%] left-0 w-full h-[50px] bg-[#fff] rounded-[5px] shadow-md p-[10px] hidden">
-                    Test
+                <div ref={searchResultRef} className="absolute top-[120%] left-0 w-full bg-[#fff] rounded-[5px] shadow-md hidden max-h-[350px] overflow-y-scroll">
+                    <div className="p-[10px] grid grid-cols-1 gap-[10px]">
+                        {renderResult()}
+                    </div>
                 </div>
             </form>
         </FormProvider>
